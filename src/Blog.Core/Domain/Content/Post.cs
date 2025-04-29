c0d17ce652e741b3d3b69eef9bc55464aa779d2a
NguyenTuanKiet <nguyentuankiet309203@gmail.com>
2025-04-29 20:34:21 +0700

    Create Entity And Migrate DB

---
+++ src/Blog.Core/Domain/Content/Post.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Domain.Content
+{
+    [Table("Post")]
+    [Index(nameof(Slug),IsUnique = true)]
+    public class Post
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public required string Name { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public required string Slug { get; set; }
+        [MaxLength(500)]
+        public string? Description { get; set; }
+        [Required]
+        public Guid CategoryId { get; set; }
+        [MaxLength(500)]
+        public string? Thumbnail { get; set; }
+        public string? Content { get; set; }
+
+        [MaxLength(500)]
+        public Guid AuthorUserId { get; set; }
+        [MaxLength(128)]
+        public string? Source { get; set; }
+        [MaxLength(250)]
+        public string? Tag {  get; set; }
+        [MaxLength(160)]
+        public string? SepDescription { get; set; }
+
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public bool IsPaid { get; set; }
+        public double? RoyaltyAmount { get; set; }
+    }
+
+    public enum PostStatus
+    {
+        Draft = 1,
+        Canceled=2,
+        WaitingForApproval = 3,
+        Rejected = 4,
+        WaitingForPublish = 5,
+        Published = 6,
+    }
+
+}
---
+++ src/Blog.Core/Domain/Identity/AppRole.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Domain.Identity
+{
+    [Table("AppRoles")]
+    public class AppRole : IdentityRole<Guid>
+    {
+        [Required]
+        [MaxLength(200)]
+        public required string DisplayName {  get; set; }
+    }
+}
